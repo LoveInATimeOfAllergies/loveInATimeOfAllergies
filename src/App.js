0f68b6208b4c3e35adb03e9e6ba5d8d50575561e
leohon <leo.cl.hon@gmail.com>
2022-12-17 15:32:35 -0500

    put form into section and commented potential form + guestlist layout

--- src/App.js
+++ src/App.js
@@ -16,12 +16,10 @@ function App() {
   const dbRef = ref(database);
   // Setting up states
   const [partyInput, setPartyInput] = useState("");
-  
   //////// user-input states //////////////
   const [guest, setGuest] = useState([]);
   const [partyDataList, setPartyDataList] = useState([]);
   const [userChoice, setUserChoice] = useState("");
-  // const [filtered, setFiltered] = useState([]);
 
   const [recipes, setRecipes] = useState([]);
 
@@ -79,31 +77,36 @@ function App() {
     <div className="App">
       <Header />
       <main>
-        <PartyForm
-          partyInput={partyInput}
-          setPartyInput={setPartyInput}
-        />
+        <section className="wrapper">
+          {/* <div> */}
+            <PartyForm
+              partyInput={partyInput}
+              setPartyInput={setPartyInput}
+            />
 
-        <Checkboxes
-          partyInput={partyInput}
-        />
-        
-        <Dropdown
-          userChoice={userChoice}
-          setUserChoice={setUserChoice}
-          partyDataList={partyDataList}
-          guest={guest}
-          setRecipes={setRecipes}
-          // filtered={filtered}
-          // setFiltered={setFiltered}
-        />
-        
-        <GuestList
-          guest={guest}
-          userChoice={userChoice}
-          // filtered={filtered}
-        />
-        
+            <Checkboxes
+              partyInput={partyInput}
+            />
+          {/* </div> */}
+        </section>
+          {/* <div> */}
+          <Dropdown
+            userChoice={userChoice}
+            setUserChoice={setUserChoice}
+            partyDataList={partyDataList}
+            guest={guest}
+            setRecipes={setRecipes}
+            // filtered={filtered}
+            // setFiltered={setFiltered}
+          />
+          
+          <GuestList
+            guest={guest}
+            userChoice={userChoice}
+            // filtered={filtered}
+          />
+          {/* </div> */}
+        {/* </section> */}
         <RecipeDisplay
           recipes={recipes}
         />
