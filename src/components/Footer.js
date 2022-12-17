7cbeeb5fe83cf34cf31884cfb5d333f3d9cfe53a
LucPasq <lucaspasq@gmail.com>
2022-12-17 14:17:31 -0500

    initial experimentation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
+import '../headerFooter.css'
+
 const Footer = () => {
     return (
         <footer>
-            <p>Created at Juno College of Technology</p>
+            <p>Created at Juno College of Technology 2022</p>
+            <p>App built by Daniela, Leo, Gogulan and Lucas</p>
         </footer>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,10 @@ const Header = () => {
     return (
         <header>
             <h1>Love in a Time of Allergies</h1>
-            {/* Something that explains what the app is about */}
+            <p>Having a dinner party, but unsure of what to cook for your friends? Concerned about creating the PERFECT dish for your allergy-stricken guests? Looking to experiment with your culinary options?</p>
+            <p>You're in the right place! Simply name a party, then type in your guest names, alongside their dietary restrictions, and our app will search for recipes that cater to everyone's tastes, no matter what they are!</p>
+            <p>Hosting has never been as stress-free, simple, and easy, allowing you to focus on doing the things that you love!</p>
+            <p>After all, who doesn't want Love In a Time Of Allergies?</p>
         </header>
     )
 }
