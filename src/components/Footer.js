306ea2703004bc6c8e2e4ab4c8c132957ef1988b
LucPasq <lucaspasq@gmail.com>
2022-12-18 13:10:44 -0500

    fixed ancho tags, whoopsie!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer>
             <p>Created at Juno College of Technology 2022</p>
-            <p>App built by Daniela, Leo, Gogulan and Lucas</p>
+            <p>App built by <a href='#'>Daniela .P</a>, <a href='#'> Leo .H </a> , <a href='#'> Gogulan .R </a> and <a href='#'> Lucas .P </a> </p>
         </footer>
     )
 }
