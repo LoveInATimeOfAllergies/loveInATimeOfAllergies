2343da1f8344e2001caa96fcf239af10d412b34f
leohon <leo.cl.hon@gmail.com>
2022-12-18 16:18:49 -0500

    added our GitHub profiles to a tag :sponge:

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <p>Created at <a href="https://junocollege.com/">Juno College of Technology</a> 2022</p>
-            <p>App built by <a href='#'>Daniela P</a>, <a href='#'>Leo H</a>, <a href='#'>Gogulan R</a>, and <a href='#'>Lucas P</a></p>
+            <p>App built by <a href='https://github.com/parradaniela'>Daniela P</a>, <a href='https://github.com/leohon'>Leo H</a>, <a href='https://github.com/Gogulan12'>Gogulan R</a>, and <a href='https://github.com/LucPasq'>Lucas P</a></p>
         </footer>
     )
 }
